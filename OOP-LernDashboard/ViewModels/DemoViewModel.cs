a05c50b85483abbdfd815a522e88914ab24291f0
Foffa <schmuck.christopher@gmail.com>
2024-04-19 17:23:10 +0200

    Update Demo

--- OOP-LernDashboard/ViewModels/DemoViewModel.cs
+++ OOP-LernDashboard/ViewModels/DemoViewModel.cs
@@ -2,11 +2,27 @@
 {
     internal class DemoViewModel : ViewModelBase
     {
-        public string? TextBoxInput { get; set; }
+        private string? _textBoxInput;
+        public string? TextBoxInput 
+        { 
+            get { return _textBoxInput; }
+            set
+            {
+                _textBoxInput = value;
+                OnPropertyChanged(nameof(TextBoxInput));
+            }
+
+        }
 
         public DemoViewModel()
         {
 
+        }
+
+        public static DemoViewModel LoadViewModel()
+        {
+            DemoViewModel viewModel = new DemoViewModel();
+            return viewModel;
         }
 
     }
