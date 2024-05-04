58a9152151fc27dbe5492224cbc44bd07615d0ae
FelixLttks <luttkusfelix@gmail.com>
2024-05-04 19:03:48 +0200

    added recurringToDo scaffolding

---
+++ OOP-LernDashboard/Models/RecurringToDo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_LernDashboard.Models
+{
+    internal class RecurringToDo : ToDo
+    {
+        public DateTime? NextDate { get; set; }
+        public int Interval { get; set; }
+
+        public RecurringToDo(string description, int interval) : base(description)
+        {
+            this.Interval = interval;
+        }
+    }
+}
