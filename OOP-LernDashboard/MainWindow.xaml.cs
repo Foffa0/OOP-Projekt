6f642fef6ee355e9f2d14c35945f5fc0903d2ebd
Foffa <schmuck.christopher@gmail.com>
2024-05-13 09:59:08 +0200

    Textbox focus workaround

--- OOP-LernDashboard/MainWindow.xaml.cs
+++ OOP-LernDashboard/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace OOP_LernDashboard
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace OOP_LernDashboard
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +11,18 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            TextBox textBox = new TextBox();
+            if (FocusManager.GetFocusedElement(this) != null)
+            {
+                if (FocusManager.GetFocusedElement(this).GetType() == textBox.GetType())
+                {
+                    Keyboard.ClearFocus();
+                }
+            }
         }
     }
 }
