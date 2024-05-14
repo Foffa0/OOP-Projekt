06d67f1592a4c452ebc32356a02f66aad07946a3
Foffa <schmuck.christopher@gmail.com>
2024-05-14 18:52:20 +0200

    Fixed database migrations not working

---
+++ OOP-LernDashboard/DbContexts/DashboardDesignTimeDBContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace OOP_LernDashboard.DbContexts
+{
+    class DashboardDesignTimeDBContextFactory : IDesignTimeDbContextFactory<DashboardDbContext>
+    {
+        public DashboardDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptions options = new DbContextOptionsBuilder().UseSqlite("Data Source=dashboard.db").Options;
+
+            return new DashboardDbContext(options);
+        }
+    }
+}
---
+++ OOP-LernDashboard/Migrations/20240514165032_Shortcuts.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OOP_LernDashboard.Migrations
+{
+    /// <inheritdoc />
+    public partial class Shortcuts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsChecked",
+                table: "ToDos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "ToDos",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Shortcuts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    Path = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    IconPath = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shortcuts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shortcuts");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsChecked",
+                table: "ToDos",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "ToDos",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
