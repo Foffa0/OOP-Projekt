db689bfc8a07ab437e2ec3acb58da082608f40ef
lucas <lucas.horn.3@web.de>
2024-06-01 16:19:49 +0200

    Binding QuickNotesView to QuickNotesViewModel and Add ObservableCollection

---
+++ OOP-LernDashboard/ViewModels/QuickNoteViewModel.cs
@@ -0,0 +1,23 @@
+﻿using OOP_LernDashboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace OOP_LernDashboard.ViewModels
+{
+    internal class QuickNoteViewModel : ViewModelBase
+    {
+        
+        private QuickNote _quickNote { get; }
+
+        public string? Note => _quickNote.Note;
+
+        public QuickNoteViewModel(QuickNote quickNote)
+        {
+            _quickNote = quickNote;
+        }
+    }
+}
--- OOP-LernDashboard/ViewModels/QuickNotesViewModel.cs
+++ OOP-LernDashboard/ViewModels/QuickNotesViewModel.cs
@@ -1,12 +1,35 @@
 ﻿using OOP_LernDashboard.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace OOP_LernDashboard.ViewModels
 {
     internal class QuickNotesViewModel : ViewModelBase
     {
-        private QuickNote _quickNote { get; }
+        private readonly ObservableCollection<QuickNoteViewModel> _quickNotes;
+        public IEnumerable<QuickNoteViewModel> QuickNotes => _quickNotes;
 
-        public string? Note => _quickNote.Note;
+        private string _note = "";
+        public string Note
+        {
+            get { return _note; }
+            set
+            {
+                _note = value;
+                OnPropertyChanged(nameof(Note));
+            }
+        }
+
+        public ICommand AddQuickNoteCommand;
+        public ICommand DeleteQuickNoteCommand;
+
+        public QuickNotesViewModel()
+        {
+            _quickNotes = new ObservableCollection<QuickNoteViewModel>();
+            _quickNotes.Add(new QuickNoteViewModel(new QuickNote("sdafjhlöasdlkökjfhölsdalkjfdlkösa")));
+            _quickNotes.Add(new QuickNoteViewModel(new QuickNote("sdafjhlöasdlkökjssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssfhölsdalkjfdlkösa")));
+            _quickNotes.Add(new QuickNoteViewModel(new QuickNote("sdafjasdfffffffdddddddddddddddddddddddddddddddddddddddddddddddddddddddddhlöasdlkökjfhölsdalkjfdlkösa")));
+        }
 
         public static QuickNotesViewModel LoadViewModel()
         {
