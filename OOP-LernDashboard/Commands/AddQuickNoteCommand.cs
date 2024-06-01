c1800c52e58b36b1bd0ac8dcba7e54eb276f1493
lucas <lucas.horn.3@web.de>
2024-06-01 13:50:24 +0200

    Binding QuickNotesView to QuickNotesViewModel

---
+++ OOP-LernDashboard/Commands/AddQuickNoteCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_LernDashboard.Commands
+{
+    internal class AddQuickNoteCommand : CommandBase
+    {
+        public override void Execute(object? parameter)
+        {
+            
+        }
+    }
+}
