485965013c380f67ff3449e51888f0096a49f154
BenForklifter <benst7@web.de>
2024-06-05 20:42:03 +0200

    Add recurring ToDo stuff to View without logic

--- OOP-LernDashboard/ViewModels/ToDosViewModel.cs
+++ OOP-LernDashboard/ViewModels/ToDosViewModel.cs
@@ -32,6 +32,80 @@ namespace OOP_LernDashboard.ViewModels
                 OnPropertyChanged(nameof(IsRecurringToDo));
             }
         }
+        private bool _startTimeIsNow = false;
+        public bool StartTimeIsNow
+        {
+            get { return _startTimeIsNow; }
+            set
+            {
+                _startTimeIsNow = value;
+                OnPropertyChanged(nameof(StartTimeIsNow));
+            }
+        }
+        private DateTime _newToDoStartTime= DateTime.Now;
+        public DateTime NewToDoStartTime
+        {
+            get { return _newToDoStartTime; }
+            set
+            {               
+                _newToDoStartTime = value;
+                OnPropertyChanged(nameof(NewToDoStartTime));
+            }
+        }
+        private DateTime _newToDoDate = DateTime.Today;
+        public DateTime NewToDoDate
+        {
+            get { return _newToDoDate; }
+            set
+            {
+                _newToDoDate = value;
+                OnPropertyChanged(nameof(NewToDoDate));
+            }
+        }
+        private int _intervalHours;
+        public int IntervalHours
+        {
+            get => _intervalHours;
+            set
+            {
+                _intervalHours = value;
+                OnPropertyChanged(nameof(IntervalHours));
+
+            }
+        }
+        private int _intervalDays;
+        public int IntervalDays
+        {
+            get => _intervalDays;
+            set
+            {
+                _intervalDays = value;
+                OnPropertyChanged(nameof(IntervalDays));
+
+            }
+        }
+        private int _intervalMonths;
+        public int IntervalMonths
+        {
+            get => _intervalMonths;
+            set
+            {
+                _intervalMonths = value;
+                OnPropertyChanged(nameof(IntervalMonths));
+
+            }
+        }
+        private int _intervalYears;
+        public int IntervalYears
+        {
+            get => _intervalYears;
+            set
+            {
+                _intervalYears = value;
+                OnPropertyChanged(nameof(IntervalYears));
+
+            }
+        }
 
         public ICommand LoadDataAsyncCommand { get; }
         public ICommand AddToDoCommand { get; }
